8b7e0d4eaf76e31b7f78829eb80cc05625c9ae4e
Mahip <kcmahip@gmail.com>
2019-01-18 15:58:18 +0530

    Corrected a minor motor logic.

--- lf_motor.py
+++ lf_motor.py
@@ -36,7 +36,7 @@ def callback(data):
 
     else:
         print ("FORWARD")
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(18, GPIO.HIGH)
         GPIO.output(17, GPIO.LOW)
         GPIO.output(27,GPIO.HIGH)
         GPIO.output(22,GPIO.LOW)        
